7087f08eb926c1c798b60020c127efad5d5d2a73
Xsufu <aluno.macarrao@gmail.com>
2022-03-27 14:36:16 +0700

    Написан метод получения скреблированного слова

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,18 +4,56 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
-    init {
-        Log.d("GameFragment", "GameViewModel created!")
-    }
+    private var _score = 0
+    val score: Int
+        get() = _score
 
-    private var score = 0
-    private var currentWordCount = 0
-    private var _currentScrambledWord = "test"
+    private var _currentWordCount = 0
+    val currentWordCount: Int
+        get() = _currentWordCount
+
+    private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
         get() = _currentScrambledWord
 
-    override fun onCleared() {
-        super.onCleared()
-        Log.d("GameFragment", "GameViewModel destroyed!")
+    // Список слов, использованных в игре
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+    // Вызов функции отображения скремблированного слова при инициализации
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
+    }
+
+    /*
+    * Обновляет currentWord и currentScrambledWord следующим словом
+    */
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+
+        val tempWord = currentWord.toCharArray()
+        while (String(tempWord).equals(currentWord, false)){
+            tempWord.shuffle()
+        }
+
+        if (wordsList.contains(currentWord)){
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++_currentWordCount
+            wordsList.add(currentWord)
+        }
+    }
+
+    /*
+    * Возвращает true, если счётчик текущего слова меньше, чем MAX_NO_OF_WORDS
+    * Обновляет следующее слово
+    */
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
     }
 }
