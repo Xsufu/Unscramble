fb8dd833edcb5cc1bc89189ebc8a0dd1a6348596
Xsufu <aluno.macarrao@gmail.com>
2022-03-27 15:15:57 +0700

    Реализована работа кнопки "Пропустить"

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -107,7 +107,12 @@ class GameFragment : Fragment() {
      * Увеличивает счётчик слов
      */
     private fun onSkipWord() {
-
+        if (viewModel.nextWord()) {
+            setErrorTextField(false)
+            updateNextWordOnScreen()
+        } else {
+            showFinalScoreDialog()
+        }
     }
 
     /*
